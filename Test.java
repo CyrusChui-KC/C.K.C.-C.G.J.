7e939823042cb2e4e3599c82657d2a1d4fe0a78b
HOSHIZATO KIRA YUME <cyruschuikc@gmail.com>
2025-09-03 16:53:11 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestClass {
+public class Main {
   public static void main(String[] args) {
     System.out.println("Integer Number 1:");
     // keyboard input
@@ -9,12 +9,12 @@ public class TestClass {
     System.out.println("Integer Number 2:");
     int num2 = kb.nextInt();
     int temp = num1 + num2;
-    System.out.println("The total = " + temp)
+    System.out.println("The total = " + temp);
     if (num1 < 18) {
       System.out.println("Go home watch cartoon!");
     }
     else{
-      System.out.println("Welcome to cinema!")
+      System.out.println("Welcome to cinema!");
     }
     for (int i = 0; i < 3; i++) {
       System.out.println("Testing...");
